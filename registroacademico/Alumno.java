12544e077141219c5c2051ccc29cf36e0e30d35b
KevinJose15 <mt25001@esfe.agape.edu.sv>
2025-05-21 16:21:58 -0600

    tercero

--- registroacademico/Alumno.java
+++ registroacademico/Alumno.java
@@ -1,5 +1,42 @@
 package registroacademico;
 
-public class Alumno {
+public class Alumno extends DatoGeneral{
+    private String nCarnet;
+    private String encargado;
+
+    public Alumno() {
+    }
+    public Alumno(String nombre, String direccion, String telefono, String email, String nCarnet, String encargado) {
+        super(nombre, direccion, telefono, email);
+        this.nCarnet = nCarnet;
+        this.encargado = encargado;
+    }
+    public String getnCarnet() {
+        return nCarnet;
+    }
+    public void setnCarnet(String nCarnet) {
+        this.nCarnet = nCarnet;
+    }
+    public String getEncargado() {
+        return encargado;
+    }
+    public void setEncargado(String encargado) {
+        this.encargado = encargado;
+    }
+
+    public void verNotas() {
+        System.out.println("Notas del alumno " + getNombre() + ":");
+        // Aquí se pueden mostrar las notas del alumno
+    }
+    public void entregarTareas() {
+        System.out.println("Entrega de tarea del alumno " + getNombre() + ":");
+        // Aquí se puede implementar la lógica para entregar una tarea
+    }
+    @Override
+    public void registrar() {
+        System.out.println("Registro de alumno:");
+        System.out.println("Número de carnet: " + nCarnet);
+        System.out.println("Encargado: " + encargado);
+    }
 
 }
--- registroacademico/Maestro.java
+++ registroacademico/Maestro.java
@@ -1,5 +1,37 @@
 package registroacademico;
 
-public class Maestro {
+public class Maestro extends DatoGeneral{
+    private String dui;
+    private String grado;
+    public Maestro() {
+    }
 
+    public Maestro(String nombre, String direccion, String telefono, String email, String dui, String grado) {
+        super(nombre, direccion, telefono, email);
+        this.dui = dui;
+        this.grado = grado;
+    }
+    public String getDui() {
+        return dui;
+    }
+    public void setDui(String dui) {
+        this.dui = dui;
+    }
+    public String getGrado() {
+        return grado;
+    }
+    public void setGrado(String grado) {
+        this.grado = grado;
+    }
+    public void pasarlista(){
+        System.out.println("El Maestro" + getNombre() + ":");
+    }
+
+    @Override
+    public void modificar() {
+        System.out.println("Modificación de maestro:");
+        System.out.println("Número de DUI: " + dui);
+        System.out.println("Grados: " + grado);
+    }
+    
 }
